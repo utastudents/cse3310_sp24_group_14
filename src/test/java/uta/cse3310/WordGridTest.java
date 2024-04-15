42b7b08f1d173d0c6179eeb01dfa828dbc92eed7
utaact5500 <158862927+utaact5500@users.noreply.github.com>
2024-04-15 16:09:47 -0500

    Update WordGridTest.java

--- src/test/java/uta/cse3310/WordGridTest.java
+++ src/test/java/uta/cse3310/WordGridTest.java
@@ -17,41 +17,41 @@ public class WordGridTest extends TestCase{
     }
 
     public void testPosition(){
-        // testing position functions to see if data is properly going from point A to point B
+        testing position functions to see if data is properly going from point A to point B
 
-        // ArrayList<String> words = new ArrayList<String>(){
-        //     {
-        //         add("cat");
-        //         add("bat");
-        //         add("rat");
-        //         add("tnt");
-        //         add("sad");
-        //         add("go");
-        //         add("rad");
-        //         add("mad");
-        //         add("fad");
-        //         add("run");
-        //         add("dun");
-        //     }
-        // };
-        // Position start = new Position(2, 3);
-        // Position end = new Position(5, 6);
-        // String word = "cat";
-        // WordGrid wg = new WordGrid(6, words);
+        ArrayList<String> words = new ArrayList<String>(){
+             {
+                 add("cat");
+                 add("bat");
+                 add("rat");
+                 add("tnt");
+                 add("sad");
+                 add("go");
+                 add("rad");
+                 add("mad");
+                 add("fad");
+                 add("run");
+                 add("dun");
+             }
+         };
+        Position start = new Position(2, 3);
+        Position end = new Position(5, 6);
+        String word = "cat";
+        WordGrid wg = new WordGrid(6, words);
 
-        // // printing for demonstration
-        // System.out.println("----------------TESTING MESSAGING----------------");
-        // wg.placeWords();
-        // wg.generateGrid();
+        // printing for demonstration
+        System.out.println("----------------TESTING MESSAGING----------------");
+        wg.placeWords();
+        wg.generateGrid();
 
-        // System.out.println(wg.checkWordSelection(start, end));
+        System.out.println(wg.checkWordSelection(start, end));
 
-        // wg.getWord(start,end);
+        wg.getWord(start,end);
         // wg.highlightWord(word);
-        // System.out.println("T/F 'cat' is in the grid\n");
-        // System.out.println(wg.isWordPlaced(word)+"\n");
-        // System.out.println("----------------TESTING COMPLETE-----------------");
+        System.out.println("T/F 'cat' is in the grid\n");
+         System.out.println(wg.isWordPlaced(word)+"\n");
+        System.out.println("----------------TESTING COMPLETE-----------------");
 
-        // messaging testing ends here
+        //messaging testing ends here
     }
 }
