58b1fcc55357b40ff53fbe2888986bb4107a6869
utaact5500 <158862927+utaact5500@users.noreply.github.com>
2024-04-14 20:00:26 -0500

    Update Position.java

--- src/main/java/uta/cse3310/Position.java
+++ src/main/java/uta/cse3310/Position.java
@@ -1,38 +1,40 @@
+//Last modified: 4/14/24 by Anthony Timberman
 package uta.cse3310;
 
 public class Position {
-    private int x;
-    private int y;
+    private int row;
+    private int col;
 
     // Constructor to set the initial position
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     // Getter for the x-coordinate
     public int getX() {
-        return x;
+        return row;
     }
 
     // Setter for the x-coordinate
     public void setX(int x) {
-        this.x = x;
+        this.row = row;
     }
 
     // Getter for the y-coordinate
     public int getY() {
-        return y;
+        return col;
     }
 
     // Setter for the y-coordinate
     public void setY(int y) {
-        this.y = y;
+        this.col = col;
     }
 
     
     @Override
     public String toString() {
-        return "(" + x + ", " + y + ")";
+        return "(" + row + ", " + col + ")";
     }
+
 }
