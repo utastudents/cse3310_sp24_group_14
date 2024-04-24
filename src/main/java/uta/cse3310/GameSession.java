169415837fed0dab7d4a9e84a1943f81cf825b1a
utaact5500 <158862927+utaact5500@users.noreply.github.com>
2024-04-24 01:18:54 -0500

    Update GameSession.java
    Tony implementation of Game Session

--- src/main/java/uta/cse3310/GameSession.java
+++ src/main/java/uta/cse3310/GameSession.java
@@ -1,139 +1,120 @@
-//Modified by Tien Dat Do 04/14/2024
-
 package uta.cse3310;
 
-import java.util.Map;
-import java.util.HashMap;
-import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
-
-//added by Uriel 4/22/2024
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
-import java.util.Collections;
-
-import org.java_websocket.WebSocket;
-import org.java_websocket.drafts.Draft;
-import org.java_websocket.drafts.Draft_6455;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.server.WebSocketServer;
-import java.util.Vector;
-import java.time.Instant;
-import java.time.Duration;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-// export HTTP_PORT = 9014;
-// export WEBSOCKET_PORT = 9114;
+import java.nio.Buffer;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.awt.Color;
 
 public class GameSession {
-    public WordGrid wordGrid;
-    private Map<Player, Integer> scores = new HashMap<>();
-    private ArrayList<Player> players;
-    private Timer timer;
-    private ChatBox chatBox;
 
-    // public GameSession(int port) {
-    //     super(new InetSocketAddress(port));
-    //   }
-    
-    //   public GameSession(InetSocketAddress address) {
-    //     super(address);
-    //   }
-    
-    //   public GameSession(int port, Draft_6455 draft) {
-    //     super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));
-    //   }
+    public ArrayList<Player> players;
+    public String[] Msg;
+    public int GameId;
+    public WordGrid Grid;
+    public ArrayList<String> Wordbank;
+    public int playercount = 0;
+    public Position[][] buttons = new Position[35][35];
+    boolean Start = false;
 
-    // @Override
-    // public void onOpen(WebSocket conn, ClientHandshake handshake) {
+    GameSession() {
+        Msg = new String[2];
+        Msg[0] = "Ready Up to Begin Game!";
+        Msg[1] = "";
+    }
 
-    // // connectionId++;
-
-    // System.out.println(conn.getRemoteSocketAddress().getAddress().getHostAddress() + " connected");
-
-    // // ServerEvent E = new ServerEvent();
-
-    // // search for a game needing a player
-    // // Game G = null;
-    // // for (Game i : ActiveGames) {
-    // //   if (i.Players == uta.cse3310.PlayerType.XPLAYER) {
-    // //     G = i;
-    // //     System.out.println("found a match");
-    // //   }
-    // }
 
     public void startGame() {
-        // wordGrid = selectWordGrid();
-        players = new ArrayList<>();
-        chatBox = new ChatBox();
-        // Initialize the timer for the game session (assuming a default duration of 10 minutes)
-        startTimer(600);
-    }
+        String word;
+        ArrayList<String> allwords = new ArrayList<>();
 
-    //  private WordGrid selectWordGrid() { // commented out because it is causing maven to crash please fix
-    //      int testGridIndex = Integer.parseInt(System.getenv("TEST_GRID"));
-    //      // Placeholder logic: Select a word grid based on the test grid index
-    //      WordGrid selectedGrid = WordGrid.selectGrid(testGridIndex);
-    //      return selectedGrid;
-    //  }
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("resources/words.txt"));
+            word = reader.readLine();
+            while(word != null){
+                if(word.length() > 2 && word.length() < 8)
+                allwords.add(word);
+                word = reader.readLine();
+            }
+    
+            reader.close();
+        } catch (IOException e) {
 
-    public void endGame() {
-        stopTimer();
-        displayPlayerScores();
-        showWinner(getWinner());
-        // Other tasks to conclude the game
-    }
+            e.printStackTrace();
+        }
 
-    private Player getWinner() {
-        Player winner = null;
-        int maxScore = Integer.MIN_VALUE;
-        for (Map.Entry<Player, Integer> entry : scores.entrySet()) {
-            if (entry.getValue() > maxScore) {
-                maxScore = entry.getValue();
-                winner = entry.getKey();
+
+        Grid = new WordGrid(35, allwords);
+        Wordbank = new ArrayList<>(Grid.wordsInGrid);
+        Grid.placeWords();
+        Grid.generateGrid();
+        int i, j;
+
+        for(i = 0; i < 35;i++){
+            for(j= 0; j < 35; j++){
+                buttons[i][j] = new Position(i, j);
             }
         }
-        return winner;
+
+        Start = true;
     }
 
-    public boolean isFull(){
-        // TODO: return if session is full
-        return true;
-    }
-
-    //  public boolean validateWordSelection(Position startPos, Position endPos) { // causing maven to crash please fix
-    //      // Placeholder logic: Check if the selected positions form a valid word on the grid
-    //      return wordGrid.isValidWord(startPos, endPos);
-    //  }
-
-    public void updateScores(Player player, int score) {
-        scores.put(player, scores.getOrDefault(player, 0) + score);
-    }
-
-    public void startTimer(int duration) {
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                endGame();
+    // This function returns an index for each player
+    // It does not depend on the representation of Enums
+    public int PlayerToIdx(Player P) {
+        int i;
+        for (i=0; i < 4; i++)
+            if (P == players.get(i)) {
+                return i;
             }
-        }, duration * 1000);
+        return -1;
     }
 
-    public void stopTimer() {
-        if (timer != null) {
-            timer.cancel();
-            timer = null;
-        }
-    }
+    public void Update(UserEvent U) {
 
+        //check if event game id matches this games id
+        if(U.GameId == GameId){
+            //check if positions/buttons selected match a word
+            if (Grid.checkWordSelection(U.Button1, U.Button2)){
+
+                //if word is in the grid check it's orientation
+                //Highlight all positions that apply to the word in buttons
+                int i, j;
+                if(U.Button1.getX() == U.Button2.getX()){
+                    j = U.Button1.getX();
+                    for(i = U.Button1.getY(); i < U.Button2.getY(); i++){
+                        buttons[j][i].highlight(U.PlayerIdx.getMarkColor());
+                    }
+                }
+                else if(U.Button1.getY() == U.Button2.getY()){
+                    j = U.Button1.getY();
+                    for(i = U.Button1.getX(); i < U.Button2.getX(); i++){
+                        (buttons[i][j]).highlight(U.PlayerIdx.getMarkColor());
+                    }
+                }
+                else{
+                    j = U.Button1.getY();
+                    for(i = U.Button1.getX(); i < U.Button2.getX(); i++){
+                        buttons[i][j].highlight(U.PlayerIdx.getMarkColor());
+                        j++;
+                    }
+                }
+                //award the word to the played
+                //remove the word from the grid so no one else can use it
+                
+                U.PlayerIdx.wordsFound.add(Grid.getWord(U.Button1, U.Button2));
+                Grid.wordsInGrid.remove(Grid.getWord(U.Button1, U.Button2));
+                
+            }
+                
+
+             }
+    }
     public void enterLobby(Player player) {
         // Add player to the lobby
         players.add(player);
@@ -153,87 +134,19 @@ public class GameSession {
         // Remove player from the game session
         players.remove(player);
     }
-
-    public void displayChatHistory(Player player) {
-        // Placeholder logic: Display chat history to the player
-        chatBox.displayHistory(player);
-    }
-
-    // public void censorMessage(String message) { // causing maven to crash please fix
-    //     // Placeholder logic: Censor inappropriate words in the message
-    //     String censoredMessage = chatBox.censor(message);
-    //     sendMessageToAllPlayers(censoredMessage);
-    // }
-
-    public void collapseChatBox() {
-        // Placeholder logic: Collapse the chat box
-        chatBox.collapse();
-    }
-
-    public void sendMessage(Player sender, String message) {
-        // Placeholder logic: Send a message from the sender to all players
-        chatBox.sendToAll(sender, message);
-    }
-
-    public void highlightLetter(Position position, Player player) {
-        // Placeholder logic: Highlight a letter in the grid for a player
-        wordGrid.highlightLetter(position, player);
-    }
-
-     public void highlightWord(Position startPos, Position endPos, Player player) {
-         // Placeholder logic: Highlight a word in the grid for a player
-        //  wordGrid.highlightWord(startPos, endPos, player);
-     }
-
-    public void showWinner(Player winner) {
-        // Display the winner of the game session
-        System.out.println("Winner: " + winner.getNick());
-    }
-
-    public void displayPlayerScores() {
-        // Display player scores to all players
-        for (Map.Entry<Player, Integer> entry : scores.entrySet()) {
-            Player player = entry.getKey();
-            int score = entry.getValue();
-            player.displayScore(score);
+    public boolean isFull(){
+        if(playercount > 4){
+            return true;
         }
+        return false;
     }
-
-    public class GameEndTask extends TimerTask {
-    private GameSession gameSession;
-
-    public GameEndTask(GameSession gameSession) {
-        this.gameSession = gameSession;
+    public void sendMessage(Player sender, String message) {
+        //Send a message from the sender to all players
+        Messaging broadcast = new Messaging();
+        broadcast.Broadcast(sender, players, message);
+        
     }
-
-    @Override
-    public void run() {
-        gameSession.endGame();
-    }
+     
 }
-
-    // public static void main(String args[]) {
-    //     int port = 9014;
-    //     GameSession gameSession = new GameSession();
-    //     gameSession.startGame();
-
-
-    //     String HttpPort = System.getenv("HTTP_PORT");
-    //     // int port = 9014;
-    //     if (HttpPort!=null) {
-    //         port = Integer.valueOf(HttpPort);
-    //       }
-
-    //     HttpServer H = new HttpServer(port, "./html");
-    //     H.start();
-    //     System.out.println("http Server started on port: " + port);
-
-    //     // create and start the websocket server
-    //     port = 9114;
-    //     String WSPort = System.getenv("WEBSOCKET_PORT");
-    //     if (WSPort!=null) {
-    //         port = Integer.valueOf(WSPort);
-    //       }
-    //     System.out.println("websocket Server started on port: " + port);
-    // }
-}
+// In windows, shift-alt-F formats the source code
+// In linux, it is ctrl-shift-I
