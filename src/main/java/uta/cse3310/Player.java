5334d1394fa10420c9acceb249c5f7cd7ae4d99b
kxo8643 <123436885+kxo8643@users.noreply.github.com>
2024-04-24 21:43:48 +0000

    uncommented some code

--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -7,18 +7,20 @@ import java.util.Random;
 
 public class Player {
     public String nick;
+    public int score;
     public List<String> wordsFound;
-    public Color markColor;                //player's unique highlighting color
+    public Color markColor;                //player's unique highlighting color; color attribute in another location
     //private ArrayList<Position> markCells;  //current word player is highlighting, will be implemented in html; can't do in java
     public ArrayList<Message> messages;
 
 
     public Player(String nick) {
-		Random rand = new Random(); 
+		Random rand = new Random();   //what's this for? @diganta
 
         this.nick = nick;
+        this.score = score; //update score in wordgrid.java
 		this.markColor = new Color(rand.nextInt(256),rand.nextInt(256),rand.nextInt(256));
-		this.wordsFound = new ArrayList<>();
+		this.wordsFound = new ArrayList<>();//add words to array in wordgrid.java
 		this.messages = new ArrayList<>();
     }
     
@@ -29,6 +31,14 @@ public class Player {
 
     public void setNick(String nick) {
         this.nick = nick;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 
     public ArrayList<Message> getMessages(){
